878abe27aa59da1f9f12bcb20fa8d5565033b6a6
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-23 08:35:09 +0800

    2018 Updates

---
+++ Conversions/problem5.scala
@@ -0,0 +1,8 @@
+//Problem 5: Write a program that converts an array/list to string.
+object Problem5{
+ def main(args:Array[String]){
+  var arrNumbers = Array("1", "2", "3");
+  var numbers = arrNumbers.mkString;
+  println(numbers);
+ }
+}
