fa7b8ecc17eda889c660fa5994889375084868da
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-26 07:27:18 +0800

    2018 Updates

---
+++ Conversions/problem11.scala
@@ -0,0 +1,8 @@
+//Problem 11: Write a program that converts numbers of day to minutes.
+object Problem11{
+ def main(args:Array[String]){
+  var day = 1;
+  var minutes = day * 24 * 60;
+  println(minutes);
+ }
+}
