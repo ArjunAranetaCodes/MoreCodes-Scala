b573eb64f50af5e9e25012e9b069a674ff87d11f
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-26 07:28:54 +0800

    2018 Updates

---
+++ Conversions/problem17.scala
@@ -0,0 +1,8 @@
+//Problem 17: Write a program that converts kilometer to meter.
+object Problem17{
+ def main(args:Array[String]){
+  var kilometer = 1;
+  var meter = kilometer * 1000;
+  println(meter);
+ }
+}
