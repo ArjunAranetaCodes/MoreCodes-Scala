38753bab49bf3abb2ee25d7ae5931af3ecd656a9
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-26 07:28:16 +0800

    2018 Updates

---
+++ Conversions/problem14.scala
@@ -0,0 +1,7 @@
+//Problem 14: Write a program that converts lower case to upper case characters.
+object Problem14{
+ def main(args:Array[String]){
+  var str = "abc";
+  println(str.toUpperCase());
+ }
+}
