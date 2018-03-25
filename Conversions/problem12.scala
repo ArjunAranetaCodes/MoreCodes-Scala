157355324a1ccbdd784ec506fd74aa3e74dedf6e
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-26 07:27:47 +0800

    2018 Updates

---
+++ Conversions/problem12.scala
@@ -0,0 +1,8 @@
+//Problem 12: Write a program that converts numbers of day to seconds.
+object Problem12{
+ def main(args:Array[String]){
+  var day = 1;
+  var seconds = day * 24 * 60 * 60;
+  println(seconds);
+ }
+}
