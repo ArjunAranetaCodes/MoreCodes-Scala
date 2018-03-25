df4ca6fac31867ca2e512fb3a2655e5af7e6463c
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-26 07:28:28 +0800

    2018 Updates

---
+++ Conversions/problem15.scala
@@ -0,0 +1,8 @@
+//Problem 15: Write a program that converts letters to ASCII.
+object Problem15{
+ def main(args:Array[String]){
+  var letter = 'x';
+  var ascii = letter.toInt;
+  println(ascii);
+ }
+}
