d2d6ae4b6aff177b88f78d71950522fc8a77d7fe
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-26 07:29:07 +0800

    2018 Updates

---
+++ Conversions/problem18.scala
@@ -0,0 +1,8 @@
+//Problem 18: Write a program that converts liter to milliliter.
+object Problem18{
+ def main(args:Array[String]){
+  var liter = 1;
+  var milliliter = liter * 1000;
+  println(milliliter);
+ }
+}
